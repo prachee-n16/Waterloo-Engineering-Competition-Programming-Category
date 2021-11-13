e483207bac84239df1182eecca686a4bbb6b5d2e
Prachee Nanda <p4nanda@uwaterloo.ca>
2021-11-13 12:38:42 -0500

    final file (not really useful anymore)

--- P1/file.py
+++ P1/file.py
@@ -1,9 +1,4 @@
-def time_Letter():
-    return 0
-
-
-def main():
-    f = open(r"WEC-21\P3-WEC21\P1\Test1.txt", "r")
+def time_Word(word):
     time_value_dictionary = {"a": 0,
                              "b": 0.25,
                              "c": 0.5,
@@ -32,15 +27,42 @@ def main():
                              "z": 0.75,
                              " ": 0,
                              }
-    WORDS = []
-    i = 0
+    word = "Crisp"
+    time = 0
 
+    for i in range(len(word)):
+        # add extra time for uppercase chars
+        print(word[i])
+        if word[i].isupper():
+            time += 2
+
+        if i != 0:
+            prev_letter = word[i - 1].lower()
+            cur_letter = word[i].lower()
+            # always add the same base time of the lowercase character
+            time += time_value_dictionary.get(cur_letter)[0]
+
+            # check if you need to move to another button
+            prev_key = time_value_dictionary.get(prev_letter)[1]
+            cur_key = time_value_dictionary.get(cur_letter)[1]
+            print("prev key" + str(prev_key))
+            print("cur key" + str(cur_key))
+            if prev_key == cur_key:
+                # the problem is, I dub
+                time += 0.5
+            else:
+                time += 0.25
+    print(time)
+
+
+if __name__ == "__main__":
+    # Open the file
+    f = open(r"WEC-21\P3-WEC21\P1\Test1.txt", "r")
+
+    # For each word in this file
     for x in f:
-        WORDS.append(str.strip(x))
-        WORD = str.strip(x)
-
-        for i in WORD:
-            LETTER = str(i)
-            print(time_value_dictionary.get(LETTER))
-
-        print("\n")
+        # Strip EOL character
+        word = str.strip(x)
+        # Function call to determine time in letter
+        # time_Word(WORD)
+        time_Word("dEFi")
