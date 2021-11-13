7a54651b21676a118acdc7b82f3205372a51c0bb
Gary Shen <39806680+GaryMShen@users.noreply.github.com>
2021-11-13 12:39:17 -0500

    Update Problem_1_Code.py

--- P1/Problem_1_Code.py
+++ P1/Problem_1_Code.py
@@ -1,34 +1,97 @@
-lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
+from timeit import default_timer as timer
+start = timer()
+file = open(r"Test2.txt", "r")
 
-time_value_dictionary = { "a":0, 
-                          "b":0.25,
-                          "c":0.5,
-                          "d":0,
-                          "e":0.25,
-                          "f":0.5,
-                          "g":0,
-                          "h":0.25,
-                          "i":0.5,
-                          "j":0,
-                          "k":0.25,
-                          "l":0.5,
-                          "m":0,
-                          "n":0.25,
-                          "o":0.5,
-                          "p":0,
-                          "q":0.25,
-                          "r":0.5,
-                          "s":0.75,
-                          "t":0,
-                          "u":0.25,
-                          "v":0.5,
-                          "w":0,
-                          "x":0.25,
-                          "y":0.5,
-                          "z":0.75,
-                          " ":0,
+WORDS = []
+i = 0
 
-
-
-
+time_value_dictionary = {
+    "a": [0, 2],
+    "b": [0.25, 2],
+    "c": [0.5, 2],
+    "d": [0, 3],
+    "e": [0.25, 3],
+    "f": [0.5, 3],
+    "g": [0, 4],
+    "h": [0.25, 4],
+    "i": [0.5, 4],
+    "j": [0, 5],
+    "k": [0.25, 5],
+    "l": [0.5, 5],
+    "m": [0, 6],
+    "n": [0.25, 6],
+    "o": [0.5, 6],
+    "p": [0, 7],
+    "q": [0.25, 7],
+    "r": [0.5, 7],
+    "s": [0.75, 7],
+    "t": [0, 8],
+    "u": [0.25, 8],
+    "v": [0.5, 8],
+    "w": [0, 9],
+    "x": [0.25, 9],
+    "y": [0.5, 9],
+    "z": [0.75, 9],
+    " ": [0, 0],
 }
+
+min_time_words = []
+
+# iterate through the string
+# count all upper case letters and the repeated letters
+# turn the string to all lowercase
+# compute extra
+# use this information to do math to compute total time
+
+def find_time(word):
+  
+  time = 0
+  for i in range(len(word)):
+      # add extra time for uppercase chars
+      if word[i].isupper():
+          time += 2
+      cur_letter = word[i].lower()
+      if i == 0:
+        time += time_value_dictionary.get(cur_letter)[0]
+      
+      else:
+          prev_letter = word[i - 1].lower()
+          # always add the same base time of the lowercase character
+          time += time_value_dictionary.get(cur_letter)[0]
+
+          # check if you need to move to another button
+          prev_key = time_value_dictionary.get(prev_letter)[1]
+          cur_key = time_value_dictionary.get(cur_letter)[1]
+          
+          if prev_key == cur_key:
+            #the problem is, I dub
+            time += 0.5
+          else:
+            time += 0.25 
+  return time, word;
+
+times_val = []
+words = []
+
+words_solution = []
+
+for x in file: #appending all the time values to array time
+  time = find_time(str.strip(x))
+  times_val.append(time[0])
+  words.append(time[1])
+
+min_time = times_val[0]
+for i in range(len(times_val)):
+  if times_val[i] < min_time:
+    min_time = times_val[i]
+    words_solution = []
+    words_solution.append(words[i])
+  elif times_val[i] == min_time:
+    words_solution.append(words[i])
+
+print(' '.join(map(str, words_solution)))
+print(min_time, 'seconds')
+
+    
+end = timer()
+print("Time taken: ", end - start)
